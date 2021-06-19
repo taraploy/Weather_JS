edaf4f66082349407f50491631593981f49ef472
Ploy Bush <tara.ployy@gmail.com>
2021-06-19 16:07:11 -0600

    Fixed temp conversion

--- src/app.js
+++ src/app.js
@@ -1,57 +1,3 @@
-
-// API
-let apiKey = "91a36f02a0aeea3d3c4881c6580e425f";
-
-function displayWeather(response) {
-  let location = document.querySelector("#location");
-  let temperature = document.querySelector("#currentTemp");
-  let description = document.querySelector("#description");
-  let icon = document.querySelector("#currentIcon");
-  let humidity = document.querySelector("#humidity");
-  let wind = document.querySelector("#wind");
-  location.innerHTML = response.data.name;
-  temperature.innerHTML = Math.round(response.data.main.temp);
-  description.innerHTML = response.data.weather[0].description;
-  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  icon.setAttribute("alt", response.data.weather[0].description);
-  humidity.innerHTML = response.data.main.humidity;
-  wind.innerHTML = Math.round(response.data.wind.speed);
-  // console.log(response.data);
-}
-
-function searchLocation(location) {
-  let unit = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayWeather);
-}
-
-// Search current location
-function searchCurrentLocation(location) {
-  let unit = "imperial";
-  let currentLocationApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${apiKey}&units=${unit}`;
-  axios.get(currentLocationApiUrl).then(displayWeather);
-}
-
-// To get current location
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
-}
-
-// Search location box
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input").value;
-  searchLocation(searchInput);
-});
-
-let currentLocationBtn = document.querySelector("#current-btn");
-currentLocationBtn.addEventListener("click", getCurrentLocation);
-
-// Default search
-searchLocation("Paris");
-
 // DATE
 
 let todayDate = document.querySelector("#todayDate");
@@ -90,30 +36,91 @@ function get_AM_PM(hour) {
 // Display Date
 todayDate.innerHTML = `${day} ${hour}:${minute} ${AM_PM}`;
 
-// Conversion
-let toFahrenheit = (event) => {
-  event.preventDefault();
-  let celcius = 19
-  let temp = Math.floor(celcius * (9 / 5) + 32);
-  f_temp.style.fontWeight = 'bold';
-  c_temp.style.fontWeight = 'normal';
-  current_temp.innerHTML = `${temp}`;
+// API
+let apiKey = "91a36f02a0aeea3d3c4881c6580e425f";
+
+function displayWeather(response) {
+  let location = document.querySelector("#location");
+  let current_temp = document.querySelector("#currentTemp");
+  let description = document.querySelector("#description");
+  let icon = document.querySelector("#currentIcon");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  location.innerHTML = response.data.name;
+  temperature = response.data.main.temp
+  current_temp.innerHTML = Math.round(temperature);
+  description.innerHTML = response.data.weather[0].description;
+  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  icon.setAttribute("alt", response.data.weather[0].description);
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  // console.log(response.data);
 }
 
-let toCelcius = (event) => {
+
+let getFahrenheit = (event) => {
   event.preventDefault();
-  let fahrenheit = 66
-  let temp = Math.floor((fahrenheit - 32) * (5 / 9));
-  c_temp.style.fontWeight = 'bold';
-  f_temp.style.fontWeight = 'normal';
-  current_temp.innerHTML = `${temp}`;
+  let celcius = document.querySelector("#currentTemp");
+
+  f_temp.classList.add("fw-bold");
+  c_temp.classList.remove("fw-bold");
+
+  celcius.innerHTML = `${Math.round(temperature)}`;
 }
+
+// Conversion
+let getCelcius = (event) => {
+  event.preventDefault();
+  let fahrenheit = document.querySelector("#currentTemp");
+
+  f_temp.classList.remove("fw-bold");
+  c_temp.classList.add("fw-bold");
+
+  let temp = Math.floor((temperature - 32) * (5 / 9));
+
+  fahrenheit.innerHTML = `${temp}`;
+}
+
+// Search current location
+function searchCurrentLocation(location) {
+  let unit = "imperial";
+  let currentLocationApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${apiKey}&units=${unit}`;
+  f_temp.classList.add("fw-bold");
+  c_temp.classList.remove("fw-bold");
+  axios.get(currentLocationApiUrl).then(displayWeather);
+}
+
+// To get current location
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+}
+
+function searchLocation(location) {
+  let unit = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+// Search location box
+let searchForm = document.querySelector("#search-form");
+// Handle the form submit
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input").value;
+  searchLocation(searchInput);
+});
+
+let currentLocationBtn = document.querySelector("#current-btn");
+currentLocationBtn.addEventListener("click", getCurrentLocation);
+
+let temperature = null;
 
 let f_temp = document.querySelector("#f-temp");
+f_temp.addEventListener("click", getFahrenheit)
 let c_temp = document.querySelector("#c-temp");
-let current_temp = document.querySelector("#currentTemp");
+c_temp.addEventListener("click", getCelcius)
 
-f_temp.addEventListener("click", toFahrenheit)
-
-c_temp.addEventListener("click", toCelcius)
+// Default search
+searchLocation("Salt Lake City");
 
